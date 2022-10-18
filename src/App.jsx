75e6d547e12a5673de8ee520ea8dd2ce5c402fec
Jime Matthies <JimeMatthies@gmail.com>
2022-10-18 10:11:30 -0300

    Views Working

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
 import React, { useState } from "react";
-import Footer from "./components/Footer";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import Home from "./views/Home";
+import About from "./views/About";
+import Municipality from "./views/Municipality";
+import People from "./views/People";
+import Footer from "./components/Footer";
 
 const App = ({ copyright }) => {
-  return (
-    <>
-      <Footer footer={copyright} />
-      <Navbar />
-    </>
-  );
+    return (
+        <BrowserRouter>
+            <Navbar />
+            <Routes>
+                <Route path="/" element={<Home/>} />
+                <Route path="/about" element={<About/>} />
+                <Route path="/municipality" element={<Municipality/>} />
+                <Route path="/people" element={<People/>} />
+            </Routes>
+            <Footer footer={copyright} />
+        </BrowserRouter>
+    );
 };
 
 export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,32 +1,33 @@
 import img from "../img/PHB_logo.png";
 import React, { useRef } from "react";
+import { Link } from "react-router-dom"
 import { FaBars, FaTimes } from "react-icons/fa";
-import "../components/Navbar.css";
+import "../styles/Navbar.css";
 
 function Navbar() {
-  const navRef = useRef();
+    const navRef = useRef();
 
-  const showNavbar = () => {
-    navRef.current.classList.toggle("responsive_nav");
-  };
+    const showNavbar = () => {
+        navRef.current.classList.toggle("responsive_nav");
+    }
 
-  return (
-    <header>
-      <img className="imgLogo" src={img} />
-      <nav ref={navRef}>
-        <a href="/#">Home</a>
-        <a href="/#">info</a>
-        <button className="buttonMuni">Municipality</button>
-        <button className="buttonPerson">People</button>
-        <button className="nav-btn nav-close-btn" onClick={showNavbar}>
-          <FaTimes />
-        </button>
-      </nav>
-      <button className="nav-btn" onClick={showNavbar}>
-        <FaBars />
-      </button>
-    </header>
-  );
+    return (
+        <header>
+            <Link to="/"><img className="imgLogo" src={img} /></Link>
+            <nav ref={navRef}>
+                <Link to="/">Home</Link>
+                <Link to="/about">About</Link>
+                <Link to="/municipality"><button className="buttonMuni">Municipality</button></Link>
+                <Link to="/people"><button className="buttonPerson">People</button></Link>
+                <button className="nav-btn nav-close-btn" onClick={showNavbar}>
+                    <FaTimes />
+                </button>
+            </nav>
+            <button className="nav-btn" onClick={showNavbar}>
+                <FaBars />
+            </button>
+        </header>
+    );
 }
 
 export default Navbar;
