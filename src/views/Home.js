a51926d59a4c19bf7b9a2cc9d7b0123d527bfd66
Jime Matthies <JimeMatthies@gmail.com>
2022-10-20 19:39:59 -0300

    View Home tested

--- src/views/Home.js
+++ src/views/Home.js
@@ -7,41 +7,75 @@ const Home = () => {
     document.body.style.backgroundColor = "#1E4A5F";
     return (
         <div className="home">
-            <div className="row">
-                <div className="col-md-6 showcase">
-                    <Showcase />
+            <div className="row rowhome">
+                <div className="col-md-5 showcase d-flex justify-content-center">
+                    <div className="showcase-carousel">
+                        <Showcase />
+                    </div>
                 </div>
-                <div className="col-md-6 homeinterest">
-                    <div className="row">
-                        <div className="col-md-3 homecard">
-                            <FaCamera/>
-                            <h2>Reporta baches</h2>
+                <div className="col-md-6 homecards">
+                    <div className="row topcards d-flex justify-content-around">
+                        <div className="col-lg-3 homecard">
+                            <div className="homeicon">
+                                <FaCamera />
+                            </div>
+                            <h2>Report Pothole</h2>
                             <p>
-                                Sube tus fotos y la ubicaión de los baches que deseas que se reparen.
+                                Upload your pictures and location of pothole you wish to be repair.
                             </p>
+                            <button className="learn-more">
+                                <span className="circle" aria-hidden="true">
+                                    <span className="icon arrow"></span>
+                                </span>
+                                <span className="button-text">Learn More</span>
+                            </button>
                         </div>
-                        <div className="col-md-3 homecard">
-                            <FaCarCrash/>
-                            <h2>Evitando accidentes</h2>
+                        <div className="col-lg-3 homecard">
+                            <div className="homeicon">
+                                <FaCheck />
+                            </div>
+                            <h2>Repairs</h2>
                             <p>
-                                Es muy importante el estado de las calles para evitar accidentes de transito.
+                                We give municipality tools to make street's repairs easier.
                             </p>
+                            <button className="learn-more">
+                                <span className="circle" aria-hidden="true">
+                                    <span className="icon arrow"></span>
+                                </span>
+                                <span className="button-text">Learn More</span>
+                            </button>
                         </div>
                     </div>
-                    <div className="row">
-                        <div className="col-md-3 homecard">
-                            <FaCheck />
-                            <h2>Reparacion municipal</h2>
+                    <div className="row botcards d-flex justify-content-around">
+                        <div className="col-lg-3 homecard">
+                            <div className="homeicon">
+                                <FaMapMarkedAlt />
+                            </div>
+                            <h2>Map</h2>
                             <p>
-                                Entregamos herramientas para hacer mas fácil el trabajo municipal para reparar las calles.
+                                You can view very report on our map and filter the data.
                             </p>
+                            <button className="learn-more">
+                                <span className="circle" aria-hidden="true">
+                                    <span className="icon arrow"></span>
+                                </span>
+                                <span className="button-text">Learn More</span>
+                            </button>
                         </div>
-                        <div className="col-md-3 homecard">
-                            <FaMapMarkedAlt/>
-                            <h2>Mapa personalizado</h2>
+                        <div className="col-lg-3 homecard">
+                            <div className="homeicon">
+                                <FaCarCrash />
+                            </div>
+                            <h2>Avoid Accidents</h2>
                             <p>
-                                Puedes revisar todos los reportes y filtrarlos en nuestro mapa.
+                                Good state streets are very important to avoid traffic accidents.
                             </p>
+                            <button className="learn-more">
+                                <span className="circle" aria-hidden="true">
+                                    <span className="icon arrow"></span>
+                                </span>
+                                <span className="button-text">Learn More</span>
+                            </button>
                         </div>
                     </div>
                 </div>
