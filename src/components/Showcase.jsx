f35fc0177e12d4425c9893cdbead0a08ea368bcb
Jime Matthies <JimeMatthies@gmail.com>
2022-10-20 20:05:14 -0300

    View Home, Muni, People Tested

--- src/components/Showcase.jsx
+++ src/components/Showcase.jsx
@@ -15,37 +15,37 @@ const Showcase = () => {
     }
 
     return (
-        <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
-            <div class="carousel-inner">
-                <div class="carousel-item active">
-                    <img src={showcasehome.imageURL1} class="d-block w-100" alt="Slide 1" />
-                    <div class="carousel-caption d-none d-md-block">
+        <div id="carouselExampleControls" className="carousel slide" data-bs-ride="carousel">
+            <div className="carousel-inner">
+                <div className="carousel-item active">
+                    <img src={showcasehome.imageURL1} className="d-block w-100" alt="Slide 1" />
+                    <div className="carousel-caption d-none d-md-block">
                         <h5>{showcasehome.title1}</h5>
                         <p>{showcasehome.subtitle1}</p>
                     </div>
                 </div>
-                <div class="carousel-item">
-                    <img src={showcasehome.imageURL2} class="d-block w-100" alt="Slide 2" />
-                    <div class="carousel-caption d-none d-md-block">
+                <div className="carousel-item">
+                    <img src={showcasehome.imageURL2} className="d-block w-100" alt="Slide 2" />
+                    <div className="carousel-caption d-none d-md-block">
                         <h5>{showcasehome.title2}</h5>
                         <p>{showcasehome.subtitle2}</p>
                     </div>
                 </div>
-                <div class="carousel-item">
-                    <img src={showcasehome.imageURL3} class="d-block w-100" alt="Slide 3" />
-                    <div class="carousel-caption d-none d-md-block">
+                <div className="carousel-item">
+                    <img src={showcasehome.imageURL3} className="d-block w-100" alt="Slide 3" />
+                    <div className="carousel-caption d-none d-md-block">
                         <h5>{showcasehome.title3}</h5>
                         <p>{showcasehome.subtitle3}</p>
                     </div>
                 </div>
             </div>
-            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Previous</span>
+            <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span className="visually-hidden">Previous</span>
             </button>
-            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Next</span>
+            <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+                <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                <span className="visually-hidden">Next</span>
             </button>
         </div>
     );
