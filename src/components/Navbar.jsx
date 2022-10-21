ba31a603c6d334dc01563c7cb60e468e7dda084f
sthannais <superlanoneri@gmail.com>
2022-10-21 13:41:23 -0300

    Login css

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,43 +1,47 @@
 import img from "../img/PHB_logo.png";
-import React, { useRef } from "react";
+import React from "react";
 import { Link, NavLink } from "react-router-dom";
 import Button from "./Button";
 
 function Navbar() {
-    return (
-        <nav className="navbar navbar-expand-lg bg-light">
-            <div className="container-fluid">
-                <Link to="/">
-                    <img className="imgLogo" src={img} />
-                </Link>
-                <button
-                    className="navbar-toggler"
-                    type="button"
-                    data-bs-toggle="collapse"
-                    data-bs-target="#navbarNav"
-                    aria-controls="navbarNav"
-                    aria-expanded="false"
-                    aria-label="Toggle navigation"
-                >
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarNav">
-                    <ul className="navbar-nav">
-                        <NavLink to="/" end>
-                            Home
-                        </NavLink>
-                        <NavLink to="/about">About</NavLink>
-                        <NavLink to="/login">Log In</NavLink>
-                        <Link to="/municipality">
-                            <Button className={"buttonMuni"} text={"Municipality"} />
-                        </Link>
-                        <Link to="/people">
-                            <Button className={"buttonPeople"} text={"People"} />
-                        </Link>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-    );
+  return (
+    <nav className="navbar navbar-expand-lg bg-light">
+      <div className="container-fluid">
+        <Link to="/">
+          <img className="imgLogo" src={img} />
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <NavLink className="Home" to="/" end>
+              Home
+            </NavLink>
+            <NavLink className="About" to="/about">
+              About
+            </NavLink>
+            <NavLink className="Login" to="/login">
+              Log In
+            </NavLink>
+            <Link to="/municipality">
+              <Button className={"buttonMuni"} text={"Municipality"} />
+            </Link>
+            <Link to="/people">
+              <Button className={"buttonPeople"} text={"People"} />
+            </Link>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
 }
 export default Navbar;
