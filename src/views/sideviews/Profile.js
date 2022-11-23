f1c03556e0bb4b86bc1dede7be881ccb824bd9cb
Jime Matthies <JimeMatthies@gmail.com>
2022-11-23 03:56:36 +0000

    Profile done

--- src/views/sideviews/Profile.js
+++ src/views/sideviews/Profile.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import img from "../../img/Profile.jpg"
 
 const Profile = () => {
     return (
-        <div className=" col-md-4 card mx-3 my-3" >
-            <img src="..." className="card-img-top" alt="..." />
+        <div className=" col-md-4 card mx-3 my-3" style={{ width: "250px" }}>
+            <img className="imgProfile" src={img}/>
             <div className="card-body">
                 <h5 className="card-title">User's email</h5>
                 <p className="card-text">User's information</p>
